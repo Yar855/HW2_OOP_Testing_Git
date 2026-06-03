6ec728ca2947da123b51e8303ebb0ed87c6f76b7
Ярослав Солопов <solopov.yar@yandex.ru>
2026-06-03 22:00:24 +0300

    Добавил класс DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -92,3 +92,15 @@ class ShoppingList:
         new_list = ShoppingList()
         new_list._items = self._items + other._items
         return new_list
+    
+class DietaryRecipe(Recipe):
+    def __init__(self, title: str, diet_type: str, ingredients: list[Ingredient]):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio: float):
+        new_recipe = super().scale(ratio)
+        return DietaryRecipe(new_recipe.title, self.diet_type, new_recipe.ingredients)
+
+    def __str__(self):
+        return f"[{self.diet_type}] {super().__str__()}"
