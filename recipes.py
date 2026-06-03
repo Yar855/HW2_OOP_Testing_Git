c05ff6a35b093b57dea3cdaf2e9dfa048ad84673
Ярослав Солопов <solopov.yar@yandex.ru>
2026-06-03 16:15:54 +0300

    Слегка пофиксил repr в Ingredient, полностью реализовал класс Recipe и тесты к нему

--- recipes.py
+++ recipes.py
@@ -1,7 +1,7 @@
 class Ingredient:
     def __init__(self, name: str, quantity: float, unit: str):
         self.name = name
-        self._quantity = float(quantity)
+        self.quantity = quantity
         self.unit = unit
 
     @property
@@ -10,7 +10,7 @@ class Ingredient:
     
     @quantity.setter
     def quantity(self, value):
-        if value < 0:
+        if value <= 0:
             raise ValueError("Количество должно быть положительным")
         self._quantity = float(value)
 
@@ -18,11 +18,49 @@ class Ingredient:
         return f"{self.name}: {self._quantity} {self.unit}"
     
     def __repr__(self):
-        return f"Ingredient({self.name}, {self._quantity}, {self.unit})"
+        return f"Ingredient('{self.name}', {self._quantity}, '{self.unit}')"
     
     def __eq__(self, other):
         if not isinstance(other, Ingredient):
             return False
         return self.name == other.name and self.unit == other.unit
+
+class Recipe:
+    def __init__(self, title: str, ingredients: list[Ingredient]):
+        self.title = title
+        self.ingredients = []
+        for ing in ingredients:
+            self.add_ingredient(ing)
     
+    def add_ingredient(self, ing: Ingredient):
+        for existing in self.ingredients:
+            if ing == existing:
+                existing.quantity += ing.quantity
+                break
+
+        else:
+            self.ingredients.append(ing)
+    
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return (isinstance(ratio, float) or isinstance(ratio, int)) and ratio > 0
+    
+    def scale(self, ratio: float):
+        if self.is_valid_ratio(ratio):
+            new_recipe = []
+            for existing in self.ingredients:
+                new_recipe.append(Ingredient(existing.name, existing.quantity*ratio, existing.unit))
+            return Recipe(self.title, new_recipe)
+        else:
+            raise ValueError("Множитель должен быть положительным")
+        
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        str_ingredients = ', '.join(str(n) for n in self.ingredients)
+        return f"{self.title}, ингредиенты: {str_ingredients}"
+
+    
+
 
--- test_recipes.py
+++ test_recipes.py
@@ -13,8 +13,52 @@ def test_ing_str():
 
 @pytest.mark.parametrize("other_ing, expected", [
     (Ingredient("Мука", 50, "г"), True),
-    (Ingredient("Сахар", 50, "г"), False),
-    (Ingredient("Мука", 50, "кг"), False)
+    (Ingredient("Сахар", 100, "г"), False),
+    (Ingredient("Мука", 100, "кг"), False)
 ])
 def test_ing_eq(other_ing, expected):
     assert(ing==other_ing) == expected
+
+def test_rec_init():
+    ingredients = [ing, Ingredient("Сахар", 200, "г")]
+    recipe = Recipe("Хрючево", ingredients)
+    assert recipe.title == "Хрючево"
+    assert recipe.ingredients == ingredients
+
+def test_rec_add_new():
+    ingredients = [ing, Ingredient("Сахар", 200, "г")]
+    recipe = Recipe("Хрючево", ingredients)
+    recipe.add_ingredient(Ingredient("Мука", 4, "кг"))
+    assert len(recipe) == 3
+
+def test_rec_add_ex():
+    ingredients = [ing, Ingredient("Сахар", 200, "г")]
+    recipe = Recipe("Хрючево", ingredients)
+    recipe.add_ingredient(Ingredient("Сахар", 150, "г"))
+    assert len(recipe) == 2
+    assert recipe.ingredients[1].quantity == 350
+
+def test_rec_scale():
+    ingredients = [ing, Ingredient("Сахар", 200, "г")]
+    recipe = Recipe("Хрючево", ingredients)
+    new = recipe.scale(2)
+    assert new is not recipe
+    assert new.title == recipe.title
+    assert recipe.ingredients[0].quantity == 100
+    assert new.ingredients[0].quantity == 200
+    assert recipe.ingredients[1].quantity == 200
+    assert new.ingredients[1].quantity == 400
+    with pytest.raises(ValueError):
+        recipe.scale(0)
+    with pytest.raises(ValueError):
+        recipe.scale(-1)
+
+def test_rec_len():
+    ingredients = [ing, Ingredient("Мука", 200, "г")]
+    recipe = Recipe("Хрючево", ingredients)
+    assert len(recipe) == 1
+    recipe.add_ingredient(Ingredient("Мука", 150, "г"))
+    assert len(recipe) == 1
+
+
+    
